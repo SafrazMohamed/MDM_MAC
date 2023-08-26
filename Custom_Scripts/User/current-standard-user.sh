f90c5985d2fe157a6722ae1ad93034d906ea2ef3
SafrazMohamed <safraz.safras@gmail.com>
2023-08-26 15:05:46 +0400

    Create current-standard-user.sh
    Remove Current user from  Admin group

---
+++ Custom_Scripts/User/current-standard-user.sh
@@ -0,0 +1,17 @@
+```
+#!/bin/sh
+
+adminUsers=$(dscl . -read Groups/admin GroupMembership | cut -c 18-)
+loggedInUser=$(stat -f %Su /dev/console)
+
+for user in $adminUsers
+do
+    if [ "$user" == "$loggedInUser" ]
+    then
+        dseditgroup -o edit -d $user -t user admin
+        if [ $? = 0 ]; then echo "Removed user $user from admin group"; fi
+    else
+        echo "Admin user $user left alone"
+    fi
+done
+```
